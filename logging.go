748c0a1f111b6bd3055890bd4862796e2688ed5a
Gregory Eremin <g@erem.in>
2014-07-16 03:11:10 +0700

    Display configuration summary on startup

--- logging.go
+++ logging.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stvp/rollbar"
 	"log"
 	"os"
+	"runtime"
 )
 
 var (
@@ -14,6 +15,16 @@ func SetupLogging() {
 	logger = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds)
 	rollbar.Token = Config.Rollbar
 	rollbar.Environment = Config.Env
+
+	Log("Burlesque started in %s environment", Config.Env)
+	Log("GOMAXPROCS is set to %d", runtime.GOMAXPROCS(-1))
+	Log("Storage path: %s", Config.Storage)
+	Log("Server is running at http://127.0.0.1:%d", Config.Port)
+	if Config.Rollbar == "" {
+		Log("Rollbar is not configured and disabled")
+	} else {
+		Log("Rollbar token: %s", Config.Rollbar)
+	}
 }
 
 func Log(format string, args ...interface{}) {
